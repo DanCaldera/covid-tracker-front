5a5df67dcc8133b309052bb302cdc5467599e310
Daniel <bdcaldera@gmail.com>
2021-04-28 18:14:06 -0500

    added Hero of HomePage

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from '../pages/Home'
+
+/* <nav>
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+  </ul>
+</nav> */
+
 function App() {
-  return <div className='App'></div>
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
